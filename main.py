c0d7b1824f4afb814f2f655d3357506ef8cc5565
Muneeba Mubarak <muneebamubarak99@gmail.com>
2026-06-28 21:53:42 +0530

    Initial commit for LightRAG-for-PDFs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging.config
 from pathlib import Path
 from typing import List
 import PyPDF2
-from lightrag import LightRAG, QueryParam
+from lightrag.lightrag import LightRAG, QueryParam
 from lightrag.llm.openai import gpt_4o_mini_complete, openai_embed
 from lightrag.kg.shared_storage import initialize_pipeline_status
 from lightrag.utils import logger, set_verbose_debug
